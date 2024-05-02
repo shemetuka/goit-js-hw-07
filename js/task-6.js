0529deceeaf46ee84e4b9b11327c225abf544aa4
shemetuka <Theshemetant@gmail.com>
2024-05-02 21:23:27 +0300

    update

--- js/task-6.js
+++ js/task-6.js
@@ -10,6 +10,8 @@ const destroyBtn = controls.querySelector("[data-destroy]");
 const boxesContainer = document.querySelector("#boxes");
 
 function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < amount; i++) {
     const size = 30 + i * 10;
     const box = document.createElement("div");
@@ -17,8 +19,10 @@ function createBoxes(amount) {
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    fragment.appendChild(box);
   }
+
+  boxesContainer.appendChild(fragment);
 }
 
 function destroyBoxes() {
@@ -28,11 +32,11 @@ function destroyBoxes() {
 createBtn.addEventListener("click", () => {
   const amount = parseInt(document.querySelector("input").value);
   if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
     createBoxes(amount);
     document.querySelector("input").value = "";
   } else {
     alert("Please enter a number between 1 and 100.");
   }
 });
-
 destroyBtn.addEventListener("click", destroyBoxes);
